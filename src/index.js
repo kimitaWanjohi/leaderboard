f6286a296d454dd22cb482395486700eaf97a76f
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-13 10:38:55 +0100

    Retrive game id

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './input.css';
 import './style.css';
+import {createGame} from './module/createGame.js';
+
+const getGameID = async () => {
+    const game = await createGame('My Game');
+    const gameJSON = await game.json();
+    const gameID = gameJSON['result'];
+    const regex = /ID: (\w+)/;
+    const match = regex.exec(gameID);
+
+    if (match) {
+    const id = match[1];
+    return id;
+    }
+}
