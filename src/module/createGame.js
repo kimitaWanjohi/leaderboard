fac1ce05461dcb4f06803ebbd81db5df332c9c2d
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-13 11:35:21 +0100

    Fix linters

--- src/module/createGame.js
+++ src/module/createGame.js
@@ -1,10 +1,12 @@
-export const createGame = async (game) => {
-    const result = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-        method: 'POST',
-        body: JSON.stringify({ name: game }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        }
-    })
-    return result;
-}
+const createGame = async (game) => {
+  const result = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    body: JSON.stringify({ name: game }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return result;
+};
+
+export default createGame;
--- src/module/scores.js
+++ src/module/scores.js
@@ -1,20 +1,20 @@
 export const getScores = async (gameID) => {
-    const result = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`, {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        }
-    })
-    return result;
-}
+  const result = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return result;
+};
 
 export const addScores = async (gameID, user, scores) => {
-    const result = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`, {
-        method: 'POST',
-        body: JSON.stringify({ user: user, score: scores }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        }
-    })
-    return result;
-}
+  const result = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`, {
+    method: 'POST',
+    body: JSON.stringify({ user, score: scores }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return result;
+};
